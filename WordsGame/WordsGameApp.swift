2917daf1ec1a79a004df5287cf402eace20c4ef3
Mikita <nikita_popow_1993@mail.ru>
2023-05-23 17:04:04 +0300

    Initial Commit

---
+++ WordsGame/WordsGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordsGameApp.swift
+//  WordsGame
+//
+//  Created by Никита Попов on 23.05.23.
+//
+
+import SwiftUI
+
+@main
+struct WordsGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
